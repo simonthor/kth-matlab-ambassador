0804d490b6ab92e36a2a753ae0527e6c4cd8682a
Simon Thor <simon11.thor@hotmail.se>
2021-09-26 13:02:38 +0200

    Remove Packages dir

--- Packages/colorful_lorenz.m
+++
@@ -1,6 +0,0 @@
-clf
-[x, y, z] = lorenz(28, 10, 8/3, [0 1 1.05], [0 25], 0.000001);
-cmap = parula(length(x)-1);
-for i=1:(length(x)-1)
-    line(x(i:i+1), y(i:i+1), z(i:i+1), 'Color', cmap(i, :));
-end
